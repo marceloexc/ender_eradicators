3246b219accaf44d91ab45e1bf1217f3e03cfa89
marceloexc <marceloexc@tutanota.de>
2024-11-14 13:14:15 -0600

    servo: add code for initial servo'ing

--- ee/ee.ino
+++ ee/ee.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo myServo; 
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Ender Eradicators!");
+
+
+  // todo - set pin mode 
+  myServo.attach(9); //digital pin 9
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (int pos = 0; pos <= 180; pos += 1) { 
+    Serial.print("Setting pos as: ");
+    Serial.print(pos);
+    Serial.println();
+    myServo.write(pos);
+    delay(200);                       // Waits 15ms 
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    Serial.print("Setting pos as: ");
+    Serial.print(pos);
+    Serial.println();
+    myServo.write(pos);             
+    delay(200);                      
+  }
 }
