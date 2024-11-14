e4095db3c69b3bf9a282cda8045f3d49a36592b4
marceloexc <marceloexc@tutanota.de>
2024-11-14 14:55:01 -0600

    implement state machine

--- ee/ee.ino
+++ ee/ee.ino
@@ -6,6 +6,18 @@ bool firstSectionCrossed = false;
 bool secondSectionCrossed = false;
 bool thirdSectionCrossed = false;
 
+
+int previousState1 = 0;
+int previousState2 = 0;
+int previousState3 = 0;
+int currentState1;
+int currentState2;
+int currentState3;
+bool section1Done = false;
+bool section2Done = false;
+bool section3Done = false;
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Ender Eradicators!");
@@ -16,28 +28,31 @@ void setup() {
 }
 
 void loop() {
-
-  if (digitalRead(8) == 1) {
-    firstSectionCrossed = true;
+  // Check section 1
+  currentState1 = digitalRead(8);
+  if (currentState1 != previousState1 && !section1Done) {
+    Serial.println("First section crossed");
+    section1Done = true;
+    previousState1 = currentState1;
   }
 
-  if (firstSectionCrossed) {
-    Serial.println("First section crossed!");
-
-    if (digitalRead(7) == 1) {
-      secondSectionCrossed = true;
+  // Check section 2 if section 1 is done
+  if (section1Done) {
+    currentState2 = digitalRead(7);
+    if (currentState2 != previousState2 && !section2Done) {
+      Serial.println("Second section crossed");
+      section2Done = true;
+      previousState2 = currentState2;
     }
+  }
 
-      if (secondSectionCrossed) {
-      Serial.println("Second section crossed!");
-
-        if (digitalRead(4) == 1) {
-          thirdSectionCrossed = true;
-        }
-
-        if (thirdSectionCrossed) {
-        Serial.println("Third section crossed!");
-      }
+  // Check section 3 if section 2 is done
+  if (section2Done) {
+    currentState3 = digitalRead(4);
+    if (currentState3 != previousState3 && !section3Done) {
+      Serial.println("Third section crossed");
+      section3Done = true;
+      previousState3 = currentState3;
     }
   }
 }
